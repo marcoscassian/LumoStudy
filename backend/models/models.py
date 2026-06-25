0852ee6be74001c5cae838d58941d84f72106149
Alex Bruno <alexbrunopereira28@gmail.com>
2026-06-25 09:59:12 -0300

    recuperacao de senha

--- backend/models/models.py
+++ backend/models/models.py
@@ -9,3 +9,10 @@ class Usuarios(SQLModel, table=True):
     email: EmailStr = Field(nullable=False, unique=True)
     senha_hash: str = Field(nullable=False)
     criado_em: datetime = Field(default_factory=datetime.now)
+
+
+class RecuperacaoSenha(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: EmailStr
+    token: str
+    criado_em: datetime = Field(default_factory=datetime.now)
