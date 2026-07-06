fbc4649d32c2792e0775e7c15ffae9e94696f9f1
Alex Bruno <alexbrunopereira28@gmail.com>
2026-07-06 08:26:09 -0300

    Create ProgressCard component
    Add ProgressCard component to display progress information.

--- frontend/app/trilha/components/progresscard.js
+++ frontend/app/trilha/components/progresscard.js
@@ -1,1 +1,30 @@
 
+"use client";
+
+export default function ProgressCard() {
+
+  return (
+
+    <div className="side-card">
+
+      <h3>Progresso Geral</h3>
+
+      <div className="circle">
+
+        <div className="circle-value">
+
+          58%
+
+        </div>
+
+      </div>
+
+      <p>
+        Você concluiu mais da metade da trilha.
+      </p>
+
+    </div>
+
+  );
+
+}
