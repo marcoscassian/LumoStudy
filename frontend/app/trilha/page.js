45451af90d427a3302c0c4b89670a0b17844b78b
Alex Bruno <alexbrunopereira28@gmail.com>
2026-07-06 08:23:58 -0300

    Update page.js

--- frontend/app/trilha/page.js
+++ frontend/app/trilha/page.js
@@ -1,1 +1,88 @@
 
+"use client";
+
+import "./trilha.css";
+
+import Sidebar from "./components/sidebar";
+import Header from "./components/header";
+import SubjectCard from "./components/subjectcard";
+import ProgressCard from "./components/progresscard";
+import StreakCard from "./components/streakcard";
+import RewardCard from "./components/rewardcard";
+
+export default function TrilhaPage() {
+  return (
+    <main className="dashboard">
+
+      {/* HEADER */}
+      <Header />
+
+      {/* CONTEÚDO */}
+      <div className="dashboard-body">
+
+        <Sidebar />
+
+        <section className="content">
+
+          <div className="title-area">
+            <h1>Trilha de Estudos</h1>
+            <p>Estude por módulos, complete níveis e avance na sua jornada.</p>
+          </div>
+
+          <div className="timeline">
+
+            <SubjectCard
+              color="purple"
+              image="/linguagens.png"
+              title="Linguagens, Códigos e suas Tecnologias"
+              description="Interpretação de textos..."
+              progress={58}
+              completed="24/35 concluídos"
+            />
+
+            <SubjectCard
+              color="green"
+              image="/humanas.png"
+              title="Ciências Humanas"
+              description="História, Geografia..."
+              progress={40}
+              completed="18/32 concluídos"
+            />
+
+            <SubjectCard
+              color="blue"
+              image="/matematica.png"
+              title="Matemática"
+              description="Geometria, álgebra..."
+              progress={62}
+              completed="21/33 concluídos"
+            />
+
+            <SubjectCard
+              color="yellow"
+              image="/natureza.png"
+              title="Ciências da Natureza"
+              description="Biologia, Física..."
+              progress={48}
+              completed="16/31 concluídos"
+            />
+
+          </div>
+
+        </section>
+
+        <aside className="right-column">
+
+          <ProgressCard />
+
+          <StreakCard />
+
+          <RewardCard />
+
+        </aside>
+
+      </div>
+
+    </main>
+  );
+}
